db215c0a02708052af82858bf6180bad29524284
Nadezhda Petelimova <hikari.aka.nadya@yandex.ru>
2020-04-18 16:06:05 +0300

    Fix class name

--- src/Lesson1.java
+++ src/Lesson1.java
@@ -4,7 +4,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-public class lesson1 {
+public class Lesson1 {
 
     public static void main(String[] args) {
         List<String> listNames = Arrays.asList("Tom", "John", "Mary", "Peter", "David", "Alice");
